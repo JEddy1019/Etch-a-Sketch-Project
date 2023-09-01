4cf2191f84871e175a8e41349a71c31e3883d08e
JEddy1019 <back.up.plan.1019@gmail.com>
2023-08-31 21:25:05 -0400

    Create and style one box
    Made an html file
    Made a script file
    Made a style file
    Made one small square and styled it

---
+++ script.js
@@ -0,0 +1,13 @@
+const body = document.body;
+const container = document.querySelector('.container');
+const gridBox = document.createElement('div');
+
+gridBox.setAttribute('class', 'box');
+container.append(gridBox);
+
+
+// gridBox.textContent = 'hi';
+
+
+// container.append(gridBox);
+
