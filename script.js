52533f1b6b7b49349e579de817971a1c2cba15ce
Josiah <back.up.plan.1019@gmail.com>
2023-09-06 12:54:13 -0400

    Attempt to make a hover effect on the grid
    Attempted to make a hover effect on the grid
    Tried by using the css method and js method
    Theory: It may not working because the computer
    is looking at the original html, there is no box in the original html
    just the container

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
 const body = document.body;
 const container = document.querySelector(".container");
 const gridBox = document.createElement("div");
-// const gridBox1 = document.createElement("div");
-// const gridBox2 = document.createElement("div");
+const box = document.querySelectorAll(".box");
+
+function hoverEffect() {
+  box.setAttribute("class", "hover");
+}
 
 for (let x = 0; x < 256; x++) {
-  //256
+  //16x16=256
   const gridBox = document.createElement("div");
   gridBox.setAttribute("class", "box");
   container.append(gridBox);
 }
 
-// Identify the container
+//My attempt at making a 'hover' effect with css
+// box.addEventListener(
+//   "click",
+//   hoverEffect
+// );
 
-// gridBox1.setAttribute("class", "box");
-// gridBox2.setAttribute("class", "box");
-// let box = Array(16).fill(container.append(gridBox));
+/* Theory: It is not working because the computer
+ is looking at the original html, there is no box in the original html*/
+
+// box.forEach(() => {
+//   box.addEventListener("mouseover", (e) => {
+//     box.style.color = "black";
+//   });
+// });
+
+//Discord's code, still not working
+// for (let x = 0; x < 256; x++) {
+//   //16x16=256
+//   const gridBox = document.createElement("div");
+//   gridBox.classList.add("box");
+//   container.append(gridBox);
+// }
+
+// box.forEach((gridBox) =>
+//   box.addEventListener("mouseover", (e) => box.classList.add("hover"))
+// );
