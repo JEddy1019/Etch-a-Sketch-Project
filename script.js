7db0763b125187b11e3f850d82fddd30a1e6ee42
Josiah <back.up.plan.1019@gmail.com>
2023-09-07 08:42:57 -0400

    Add a Hover effect to the grid
    Added a hover effect to the grid so that when you mouse over it turns
    black
    Cleaned up unnecessary comments

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const body = document.body;
 const container = document.querySelector(".container");
 const gridBox = document.createElement("div");
-const box = document.querySelectorAll(".box");
+// const box = document.querySelectorAll(".box");
+const box = document.getElementsByClassName("box");
 
 function hoverEffect() {
   box.setAttribute("class", "hover");
@@ -14,29 +15,8 @@ for (let x = 0; x < 256; x++) {
   container.append(gridBox);
 }
 
-//My attempt at making a 'hover' effect with css
-// box.addEventListener(
-//   "click",
-//   hoverEffect
-// );
-
-/* Theory: It is not working because the computer
- is looking at the original html, there is no box in the original html*/
-
-// box.forEach(() => {
-//   box.addEventListener("mouseover", (e) => {
-//     box.style.color = "black";
-//   });
-// });
-
-//Discord's code, still not working
-// for (let x = 0; x < 256; x++) {
-//   //16x16=256
-//   const gridBox = document.createElement("div");
-//   gridBox.classList.add("box");
-//   container.append(gridBox);
-// }
-
-// box.forEach((gridBox) =>
-//   box.addEventListener("mouseover", (e) => box.classList.add("hover"))
-// );
+for (let y = 0; y < 256; y++) {
+  box[y].addEventListener("mouseover", function hover() {
+    box[y].classList.add("hover");
+  });
+}
