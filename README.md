ee234af94d414cbc04f052392662ecc6c7cdac4b
Josiah <back.up.plan.1019@gmail.com>
2023-09-15 11:29:51 -0400

    Figure out how to manipulate css dimensions with js
    Found a viable method of changing the dimensions of the
    grid boxes

--- README.md
+++ README.md
@@ -4,4 +4,6 @@
 
 - if you want to add an event listener to an array-like element, use a loop
 
--remember to google errors if your stuck
+- remember to google errors if your stuck
+
+- when a lot of boxes are present the page slows down a lot, should look for a way to resolve that
--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const gridBox = document.createElement("div");
 const button = document.querySelector("#expand");
 const box = document.getElementsByClassName("box");
 let gridSize = 256;
+let test = 6.25;
 
 for (let x = 0; x < gridSize; x++) {
   //16x16=256
@@ -16,6 +17,11 @@ for (let y = 0; y < gridSize; y++) {
   box[y].addEventListener("mouseover", function hover() {
     box[y].classList.add("hover");
   });
+}
+
+for (let z = 0; z < gridSize; z++) {
+  box[z].style.width = test + "%";
+  box[z].style.height = test + "%";
 }
 
 function expandGrid() {}
