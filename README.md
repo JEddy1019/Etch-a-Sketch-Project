4fa85937410db5fbe8574b0485ef5dc75adf39be
Josiah <back.up.plan.1019@gmail.com>
2023-09-27 11:19:55 -0400

    Create a button to manipulate the grid
    Made a button that expands or contracts the grid depending on the input
    received
    Removed unneeded comments

--- README.md
+++ README.md
@@ -7,3 +7,7 @@
 - remember to google errors if your stuck
 
 - when a lot of boxes are present the page slows down a lot, should look for a way to resolve that
+
+- need to find a way to remove squares in container so that I can add new ones
+
+- remember that calling functions is different from calling built in methods e.g addEventListener
--- script.js
+++ script.js
@@ -3,25 +3,87 @@ const container = document.querySelector(".container");
 const gridBox = document.createElement("div");
 const button = document.querySelector("#expand");
 const box = document.getElementsByClassName("box");
-let gridSize = 256;
-let test = 6.25;
+const expand = document.querySelector("#expand");
+const baseGridSize = 256;
 
-for (let x = 0; x < gridSize; x++) {
-  //16x16=256
+//Grid creation
+for (let x = 0; x < baseGridSize; x++) {
   const gridBox = document.createElement("div");
   gridBox.setAttribute("class", "box");
   container.append(gridBox);
 }
 
-for (let y = 0; y < gridSize; y++) {
-  box[y].addEventListener("mouseover", function hover() {
-    box[y].classList.add("hover");
-  });
+//Hover effect
+function hoverEffect(baseGridSize) {
+  for (let y = 0; y < baseGridSize; y++) {
+    box[y].addEventListener("mouseover", function hover() {
+      box[y].classList.add("hover");
+    });
+  }
 }
 
-for (let z = 0; z < gridSize; z++) {
-  box[z].style.width = test + "%";
-  box[z].style.height = test + "%";
+//Initial box sizing
+for (let z = 0; z < baseGridSize; z++) {
+  box[z].style.width = 6.25 + "%";
+  box[z].style.height = 6.25 + "%";
+  hoverEffect(baseGridSize);
 }
 
-function expandGrid() {}
+// Grid deletion function
+function deleteGrid() {
+  while (container.firstChild) {
+    container.firstChild.remove();
+    console.log("hi"); //test
+  }
+}
+
+//Creates a new grid
+function createGrid(dimension) {
+  for (let a = 0; a < dimension; a++) {
+    const gridBox = document.createElement("div");
+    gridBox.setAttribute("class", "box");
+    container.append(gridBox);
+  }
+}
+
+//Used to manipulate size of boxes
+function boxSizing(size, dimension) {
+  for (let b = 0; b < dimension; b++) {
+    box[b].style.width = size + "%";
+    box[b].style.height = size + "%";
+    hoverEffect(dimension);
+  }
+}
+
+//Used to prompt the user for an input
+function inputPrompt() {
+  let answer = prompt(
+    "Please enter desired grid size, not exceeding 100. (eg 50 if want a 50x50 grid)"
+  );
+  return Number(answer);
+}
+
+//Used to square input for dimensions
+function gridDimensions(input) {
+  let amount = input * input;
+  // console.log(amount); //test
+  return amount;
+}
+
+//Used to resize boxes
+function boxResizing(input, dimension) {
+  let resize;
+  resize = 100 / input;
+  boxSizing(resize, dimension);
+}
+
+//Used to create new grid and size boxes to fit with input
+function recreateGrid(input) {
+  deleteGrid();
+  createGrid(gridDimensions(input));
+  boxResizing(input, gridDimensions(input));
+}
+
+expand.addEventListener("click", () => {
+  recreateGrid(inputPrompt());
+});
